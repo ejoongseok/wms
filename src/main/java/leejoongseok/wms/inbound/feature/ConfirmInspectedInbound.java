a726a175226985901ac0e0c2095758ff2c85577b
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-05-31 14:46:59 +0900

    InboundIdNotFoundException 추가

--- src/main/java/leejoongseok/wms/inbound/feature/ConfirmInspectedInbound.java
+++ src/main/java/leejoongseok/wms/inbound/feature/ConfirmInspectedInbound.java
@@ -1,4 +1,16 @@
 package leejoongseok.wms.inbound.feature;
 
+import leejoongseok.wms.inbound.domain.Inbound;
+import leejoongseok.wms.inbound.domain.InboundRepository;
+import leejoongseok.wms.inbound.exception.InboundIdNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class ConfirmInspectedInbound {
+    private final InboundRepository inboundRepository;
+
+    public void request(final Long inboundId) {
+        final Inbound inbound = inboundRepository.findById(inboundId)
+                .orElseThrow(() -> new InboundIdNotFoundException(inboundId));
+    }
 }
--- src/test/java/leejoongseok/wms/inbound/feature/ConfirmInspectedInboundTest.java
+++ src/test/java/leejoongseok/wms/inbound/feature/ConfirmInspectedInboundTest.java
@@ -1,14 +1,34 @@
 package leejoongseok.wms.inbound.feature;
 
+import leejoongseok.wms.inbound.domain.Inbound;
+import leejoongseok.wms.inbound.domain.InboundRepository;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Optional;
 
 public class ConfirmInspectedInboundTest {
+
+    private ConfirmInspectedInbound confirmInspectedInbound;
+    private InboundRepository inboundRepository;
+
+    @BeforeEach
+    void setUp() {
+        inboundRepository = Mockito.mock(InboundRepository.class);
+        confirmInspectedInbound = new ConfirmInspectedInbound(inboundRepository);
+    }
 
     @Test
     @DisplayName("입고를 확정한다.")
     void confirmInspectedInbound() {
-        final ConfirmInspectedInbound confirmInspectedInbound = new ConfirmInspectedInbound();
+        final long inboundId = 1L;
+        final Inbound inbound = Instancio.create(Inbound.class);
+        Mockito.when(inboundRepository.findById(inboundId)).thenReturn(Optional.of(inbound));
+
+        confirmInspectedInbound.request(inboundId);
 
     }
 }
