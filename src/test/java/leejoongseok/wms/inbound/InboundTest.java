92f341a44525b523784d861edb3b3eb6d5f1ceb1
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-05-31 11:19:32 +0900

    inbound addInboundItems fail 테스트 추가

--- src/test/java/leejoongseok/wms/inbound/InboundTest.java
+++ src/test/java/leejoongseok/wms/inbound/InboundTest.java
@@ -10,6 +10,8 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class InboundTest {
 
     //item
@@ -30,6 +32,31 @@ class InboundTest {
         final InboundItem inboundItem = createInboundItem(item, receivedQuantity, unitPurchasePrice);
 
         inbound.addInboundItems(List.of(inboundItem));
+    }
+
+    @Test
+    @DisplayName("[실패]입고에 입고 상품을 등록한다. - 입고 총액과 입고 상품 개별 총액이 일치하지 않는 경우")
+    void fail_wrong_total_amount_addInboundItems() {
+        final Item item = createItem(itemId);
+        final BigDecimal wrongTotalAmount = BigDecimal.valueOf(3000);
+        final Inbound inbound = new Inbound(orderRequestAt, estimatedArrivalAt, wrongTotalAmount);
+        final InboundItem wrongInboundItem = createInboundItem(item, receivedQuantity, unitPurchasePrice);
+
+        assertThatThrownBy(() -> {
+            inbound.addInboundItems(List.of(wrongInboundItem));
+        }).isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("입고 상품의 총 금액이 주문 금액과 일치하지 않습니다. 입고총액: 3000, 단품 합산액: 2000");
+    }
+
+    @Test
+    @DisplayName("[실패]입고에 입고 상품을 등록한다. - 입고상품이 비어있는 경우")
+    void fail_empty_inboundItems_addInboundItems() {
+        final Inbound inbound = new Inbound(orderRequestAt, estimatedArrivalAt, totalAmount);
+
+        assertThatThrownBy(() -> {
+            inbound.addInboundItems(List.of());
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입고 상품은 1개 이상이어야 합니다.");
     }
 
     private Item createItem(final long itemId) {
