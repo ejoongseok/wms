eee67d23b31042b05c7a816f7677ca7c21de2c2d
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-05-31 17:21:35 +0900

    RejectInbound 구현

--- src/main/java/leejoongseok/wms/inbound/feature/RejectInbound.java
+++ src/main/java/leejoongseok/wms/inbound/feature/RejectInbound.java
@@ -1,4 +1,28 @@
 package leejoongseok.wms.inbound.feature;
 
+import jakarta.validation.constraints.NotBlank;
+import leejoongseok.wms.inbound.domain.Inbound;
+import leejoongseok.wms.inbound.domain.InboundRepository;
+import leejoongseok.wms.inbound.exception.InboundIdNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class RejectInbound {
+    private final InboundRepository inboundRepository;
+
+    public void request(
+            final long inboundId,
+            final Request request) {
+        final Inbound inbound = inboundRepository.findById(inboundId)
+                .orElseThrow(() -> new InboundIdNotFoundException(inboundId));
+
+        inbound.reject(request.rejectionReasons);
+
+    }
+
+    public record Request(
+            @NotBlank(message = "취소 사유를 입력해주세요.")
+            String rejectionReasons
+    ) {
+    }
 }
--- src/test/java/leejoongseok/wms/inbound/feature/RejectInboundTest.java
+++ src/test/java/leejoongseok/wms/inbound/feature/RejectInboundTest.java
@@ -1,14 +1,46 @@
 package leejoongseok.wms.inbound.feature;
 
+import leejoongseok.wms.inbound.domain.Inbound;
+import leejoongseok.wms.inbound.domain.InboundRepository;
+import leejoongseok.wms.inbound.domain.InboundStatus;
+import net.datafaker.Faker;
+import net.datafaker.providers.base.BaseFaker;
+import org.instancio.Instancio;
+import org.instancio.Select;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class RejectInboundTest {
+    InboundRepository inboundRepository;
+
+    @BeforeEach
+    void setUp() {
+        inboundRepository = Mockito.mock(InboundRepository.class);
+    }
 
     @Test
     @DisplayName("입고를 거부한다.")
     void rejectInbound() {
-        final RejectInbound rejectInbound = new RejectInbound();
+        final Inbound inbound = Instancio.of(Inbound.class)
+                .supply(Select.field(Inbound::getStatus), () -> InboundStatus.ORDER_REQUESTED)
+                .create();
+        Mockito.when(inboundRepository.findById(1L)).thenReturn(Optional.of(inbound));
+
+        final RejectInbound rejectInbound = new RejectInbound(inboundRepository);
+
+        final long inboundId = 1L;
+        final String rejectionReasons = BaseFaker.instance().lorem().sentence(10);
+        final RejectInbound.Request request = new RejectInbound.Request(rejectionReasons);
+
+        rejectInbound.request(inboundId, request);
+
+        assertThat(inbound.getStatus()).isEqualTo(InboundStatus.REJECTED);
 
     }
 }
