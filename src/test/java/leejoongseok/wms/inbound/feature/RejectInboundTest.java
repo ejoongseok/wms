7c5002e9e266ed9563c23f6cb53f32ce3d5ffc0a
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-05-31 17:02:14 +0900

    create class RejectInbound

---
+++ src/test/java/leejoongseok/wms/inbound/feature/RejectInboundTest.java
@@ -0,0 +1,14 @@
+package leejoongseok.wms.inbound.feature;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RejectInboundTest {
+
+    @Test
+    @DisplayName("입고를 거부한다.")
+    void rejectInbound() {
+        final RejectInbound rejectInbound = new RejectInbound();
+
+    }
+}
