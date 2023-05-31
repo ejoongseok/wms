edeaca8986034796380c2c8be21d30b4af9f25d6
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-05-31 11:08:14 +0900

    refactoring test

--- src/test/java/leejoongseok/wms/inbound/InboundTest.java
+++ src/test/java/leejoongseok/wms/inbound/InboundTest.java
@@ -6,20 +6,30 @@ import org.instancio.Select;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 class InboundTest {
 
+    //item
+    private final long itemId = 1L;
+    //inbound
+    private final LocalDateTime orderRequestAt = LocalDateTime.now().minusDays(1);
+    private final LocalDateTime estimatedArrivalAt = LocalDateTime.now().plusDays(1);
+    private final BigDecimal totalAmount = BigDecimal.valueOf(2000);
+    //inboundItem
+    private final int receivedQuantity = 2;
+    private final BigDecimal unitPurchasePrice = BigDecimal.valueOf(1000);
+
     @Test
     @DisplayName("입고에 입고 상품을 등록한다.")
     void addInboundItems() {
-        final Item item = createItem(1L);
-        final Inbound inbound = Instancio.create(Inbound.class);
-        final InboundItem inboundItem = createInboundItem(item);
+        final Item item = createItem(itemId);
+        final Inbound inbound = new Inbound(orderRequestAt, estimatedArrivalAt, totalAmount);
+        final InboundItem inboundItem = createInboundItem(item, receivedQuantity, unitPurchasePrice);
 
         inbound.addInboundItems(List.of(inboundItem));
-
-
     }
 
     private Item createItem(final long itemId) {
@@ -28,9 +38,14 @@ class InboundTest {
                 .create();
     }
 
-    private InboundItem createInboundItem(final Item item) {
+    private InboundItem createInboundItem(
+            final Item item,
+            final int receivedQuantity,
+            final BigDecimal unitPurchasePrice) {
         return Instancio.of(InboundItem.class)
                 .supply(Select.field(InboundItem::getItem), () -> item)
+                .supply(Select.field(InboundItem::getReceivedQuantity), () -> receivedQuantity)
+                .supply(Select.field(InboundItem::getUnitPurchasePrice), () -> unitPurchasePrice)
                 .create();
     }
 }
