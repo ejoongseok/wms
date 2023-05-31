6d0bbfe0d1a86e320dad27a64b2baeda62c930a1
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-05-31 10:15:47 +0900

    Create Inbound Test

---
+++ src/test/java/leejoongseok/wms/inbound/CreateInboundTest.java
@@ -0,0 +1,13 @@
+package leejoongseok.wms.inbound;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CreateInboundTest {
+
+    @Test
+    @DisplayName("입고를 등록한다.")
+    void createInbound() {
+
+    }
+}
