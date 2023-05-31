7401ad6258da53b205e9b5dc548aaa1e02ef1c1b
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-05-31 10:26:04 +0900

    add Constructor

--- src/main/java/leejoongseok/wms/inbound/Inbound.java
+++ src/main/java/leejoongseok/wms/inbound/Inbound.java
@@ -1,4 +1,34 @@
 package leejoongseok.wms.inbound;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Inbound {
+    private final LocalDateTime orderRequestAt;
+    private final LocalDateTime estimatedArrivalAt;
+    private final BigDecimal totalAmount;
+    private Long id;
+
+    public Inbound(
+            final LocalDateTime orderRequestAt,
+            final LocalDateTime estimatedArrivalAt,
+            final BigDecimal totalAmount) {
+        final LocalDateTime today = LocalDateTime.now();
+        if (null == orderRequestAt || today.isBefore(orderRequestAt)) {
+            throw new IllegalArgumentException("발주 요청일시는 현재시간보다 과거여야 합니다.");
+        }
+        if (null == estimatedArrivalAt || today.isAfter(estimatedArrivalAt)) {
+            throw new IllegalArgumentException("예상 도착일시는 현재시간보다 미래여야 합니다.");
+        }
+        if (null == totalAmount || 0 > totalAmount.compareTo(BigDecimal.ZERO)) {
+            throw new IllegalArgumentException("총 주문 금액은 0원보다 커야 합니다.");
+        }
+        this.orderRequestAt = orderRequestAt;
+        this.estimatedArrivalAt = estimatedArrivalAt;
+        this.totalAmount = totalAmount;
+    }
+
+    public void assignId(final Long id) {
+        this.id = id;
+    }
 }
