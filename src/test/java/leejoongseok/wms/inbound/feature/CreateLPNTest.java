51d235a1c51f2307cdf425b23a8db1247635dc2d
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-01 22:19:17 +0900

    create class CreateLPN

---
+++ src/test/java/leejoongseok/wms/inbound/feature/CreateLPNTest.java
@@ -0,0 +1,14 @@
+package leejoongseok.wms.inbound.feature;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CreateLPNTest {
+
+    @Test
+    @DisplayName("입고 아이템의 LPN을 생성한다.")
+    void createLPN() {
+        final CreateLPN createLPN = new CreateLPN();
+
+    }
+}
