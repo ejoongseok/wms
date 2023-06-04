009bcc2550c1a78bbbf5653bd0696f4f367c8269
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-04 11:04:43 +0900

    rename

--- src/main/java/leejoongseok/wms/item/exception/ItemAlreadyExistsException.java
+++ src/main/java/leejoongseok/wms/item/exception/ItemAlreadyExistsException.java
@@ -2,8 +2,8 @@ package leejoongseok.wms.item.exception;
 
 import leejoongseok.wms.common.BadRequestException;
 
-public class AlreadyExistsItemException extends BadRequestException {
-    public AlreadyExistsItemException(final String itemBarcode) {
+public class ItemAlreadyExistsException extends BadRequestException {
+    public ItemAlreadyExistsException(final String itemBarcode) {
         super(String.format("이미 등록된 아이템 바코드입니다. [%s]", itemBarcode));
     }
 }
