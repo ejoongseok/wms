38b2a0e9d2f746ee02e2b8aec828dadf6132e151
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-04 11:02:30 +0900

    LPN 바코드 중복등록 API Test 추가

--- src/test/java/leejoongseok/wms/inbound/feature/CreateLPNTest.java
+++ src/test/java/leejoongseok/wms/inbound/feature/CreateLPNTest.java
@@ -7,6 +7,7 @@ import leejoongseok.wms.inbound.domain.LPNRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 
 import java.util.Optional;
 
@@ -30,5 +31,17 @@ class CreateLPNTest extends ApiTest {
         final Optional<LPN> lpn = lpnRepository.findById(1L);
 
         assertThat(lpn).isPresent();
+    }
+
+    @Test
+    @DisplayName("[실패] 입고 아이템의 LPN을 등록한다. - 동일한 LPN 바코드가 이미 존재하는 경우")
+    void fail_duplicate_createLPN() {
+        new Scenario()
+                .createItem().request()
+                .createInbound().request()
+                .confirmInspectedInbound().request()
+                .createLPN().request()
+                .createLPN().request(HttpStatus.BAD_REQUEST.value())
+        ;
     }
 }
