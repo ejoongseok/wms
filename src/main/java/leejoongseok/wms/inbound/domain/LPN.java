13a408dc4d4d11e9443c5a2d5d9175c793974a86
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-04 09:44:23 +0900

    safe delete LPN constructor

--- src/main/java/leejoongseok/wms/inbound/domain/LPN.java
+++ src/main/java/leejoongseok/wms/inbound/domain/LPN.java
@@ -10,7 +10,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
-import org.springframework.util.Assert;
 
 import java.time.LocalDateTime;
 
@@ -37,21 +36,4 @@ public class LPN {
     @Comment("입고 아이템 ID")
     private Long inboundItemId;
 
-    public LPN(
-            final String lpnBarcode,
-            final Long itemId,
-            final LocalDateTime expirationAt,
-            final Long inboundItemId) {
-        Assert.hasText(lpnBarcode, "LPN 바코드는 필수입니다.");
-        Assert.notNull(itemId, "아이템 ID는 필수입니다.");
-        Assert.notNull(expirationAt, "유통기한은 필수입니다.");
-        Assert.notNull(inboundItemId, "입고 아이템 ID는 필수입니다.");
-        if (expirationAt.isBefore(LocalDateTime.now())) {
-            throw new IllegalArgumentException("유통기한은 현재 시간보다 나중이어야 합니다.");
-        }
-        this.lpnBarcode = lpnBarcode;
-        this.itemId = itemId;
-        this.expirationAt = expirationAt;
-        this.inboundItemId = inboundItemId;
-    }
 }
