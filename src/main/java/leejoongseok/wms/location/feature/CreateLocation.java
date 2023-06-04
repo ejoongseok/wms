593347fbde886aa756f5b554652e73eaa3d7c06c
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-05 05:48:28 +0900

    add request parameter

--- src/main/java/leejoongseok/wms/location/feature/CreateLocation.java
+++ src/main/java/leejoongseok/wms/location/feature/CreateLocation.java
@@ -1,10 +1,21 @@
 package leejoongseok.wms.location.feature;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import leejoongseok.wms.location.domain.StorageType;
+import leejoongseok.wms.location.domain.UsagePurpose;
+
 public class CreateLocation {
     public void request(final Request request) {
 
     }
 
-    public record Request() {
+    public record Request(
+            @NotBlank(message = "로케이션 바코드는 필수입니다.")
+            String locationBarcode,
+            @NotNull(message = "보관 타입은 필수입니다.")
+            StorageType storageType,
+            @NotNull(message = "보관 목적은 필수입니다.")
+            UsagePurpose usagePurpose) {
     }
 }
--- src/test/java/leejoongseok/wms/location/feature/CreateLocationTest.java
+++ src/test/java/leejoongseok/wms/location/feature/CreateLocationTest.java
@@ -1,5 +1,7 @@
 package leejoongseok.wms.location.feature;
 
+import leejoongseok.wms.location.domain.StorageType;
+import leejoongseok.wms.location.domain.UsagePurpose;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,7 +17,16 @@ class CreateLocationTest {
     @Test
     @DisplayName("로케이션을 등록한다.")
     void createLocation() {
-        final CreateLocation.Request request = new CreateLocation.Request();
+        final String locationBarcode = "A1-1-1";
+        final StorageType storageType = StorageType.CELL;
+        final UsagePurpose usagePurpose = UsagePurpose.STOW;
+
+        final CreateLocation.Request request = new CreateLocation.Request(
+                locationBarcode,
+                storageType,
+                usagePurpose
+        );
+
         createLocation.request(request);
 
     }
