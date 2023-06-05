50af50206d28715680f14cfd9bceac8bb35c0c99
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-05 14:25:50 +0900

    테스트 extract method

--- src/test/java/leejoongseok/wms/location/domain/LocationTest.java
+++ src/test/java/leejoongseok/wms/location/domain/LocationTest.java
@@ -13,15 +13,31 @@ class LocationTest {
     @Test
     @DisplayName("로케이션에 LPN을 등록한다.")
     void assignLPN() {
-        final Location location = Instancio.create(Location.class);
+        final Location location = createLocation();
         final String lpnBarcode = "lpnBarcode";
-        final LPN lpn = Instancio.of(LPN.class)
-                .supply(Select.field(LPN::getLpnBarcode), () -> lpnBarcode)
-                .create();
+        final LPN lpn = createLPN(lpnBarcode);
 
         location.assignLPN(lpn);
 
+        assertLocationLPN(location, lpnBarcode, lpn);
+    }
+
+    private Location createLocation() {
+        return Instancio.of(Location.class)
+                .ignore(Select.field(Location::getLocationLPNList))
+                .create();
+    }
+
+    private LPN createLPN(final String lpnBarcode) {
+        return Instancio.of(LPN.class)
+                .supply(Select.field(LPN::getLpnBarcode), () -> lpnBarcode)
+                .create();
+    }
+
+    private void assertLocationLPN(final Location location, final String lpnBarcode, final LPN lpn) {
         final LocationLPN locationLPN = location.getLocationLPN(lpnBarcode);
         assertThat(locationLPN).isNotNull();
+        assertThat(locationLPN.getLpn()).isEqualTo(lpn);
+        assertThat(location.getLocationLPNList()).hasSize(1);
     }
 }
