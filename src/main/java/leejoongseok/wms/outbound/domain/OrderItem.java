a8bca817732185b5e5d912c47255485612b24957
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-12 16:49:05 +0900

    OrderItem itemId -> item 참조

--- src/main/java/leejoongseok/wms/outbound/domain/OrderItem.java
+++ src/main/java/leejoongseok/wms/outbound/domain/OrderItem.java
@@ -1,9 +1,15 @@
 package leejoongseok.wms.outbound.domain;
 
+import leejoongseok.wms.item.domain.Item;
 import lombok.Getter;
 
 @Getter
 public class OrderItem {
-    private Long itemId;
+    private Item item;
     private Integer orderQuantity;
+
+
+    public Long getItemId() {
+        return item.getId();
+    }
 }
