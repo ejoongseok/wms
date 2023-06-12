3bab45b771816d8f6d29537c62cdfa2cbede1658
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-12 16:36:13 +0900

    출고용 LocationLPN 목록 검증 추가

---
+++ src/main/java/leejoongseok/wms/outbound/domain/LocationLPNValidatorForOutbound.java
@@ -0,0 +1,36 @@
+package leejoongseok.wms.outbound.domain;
+
+import leejoongseok.wms.location.domain.LocationLPN;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+/**
+ * LocationLPN 재고 수량이 출고 요청 수량에 충분한지 검증하는 클래스
+ */
+public class LocationLPNValidatorForOutbound {
+    /**
+     * LocationLPN 목록 재고 수량이 출고 요청 수량에 충분한지 검증한다.
+     */
+    public void validate(final List<LocationLPN> locationLPNList, final Integer orderQuantity) {
+        Assert.notEmpty(locationLPNList, "출고 가능한지 검증할 로케이션 LPN 목록이 비어있습니다.");
+        Assert.notNull(orderQuantity, "출고 요청 수량이 비어있습니다.");
+        if (0 >= orderQuantity) {
+            throw new IllegalArgumentException("출고 요청 수량이 0보다 작거나 같습니다.");
+        }
+        final int availableInventoryQuantityForOutbound = calculateAvailableInventoryQuantity(locationLPNList);
+        if (availableInventoryQuantityForOutbound < orderQuantity) {
+            throw new IllegalArgumentException(
+                    "출고 가능한 재고가 부족합니다. " +
+                            "출고 가능한 재고 수량: " + availableInventoryQuantityForOutbound +
+                            " 출고 요청 수량: " + orderQuantity);
+        }
+    }
+
+    private int calculateAvailableInventoryQuantity(final List<LocationLPN> nonExpiredLocationLPNList) {
+        final int availableInventoryQuantity = nonExpiredLocationLPNList.stream()
+                .mapToInt(LocationLPN::getInventoryQuantity)
+                .sum();
+        return availableInventoryQuantity;
+    }
+}
---
+++ src/test/java/leejoongseok/wms/outbound/domain/LocationLPNValidatorForOutboundTest.java
@@ -0,0 +1,84 @@
+package leejoongseok.wms.outbound.domain;
+
+import leejoongseok.wms.location.domain.LocationLPN;
+import org.instancio.Instancio;
+import org.instancio.Select;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LocationLPNValidatorForOutboundTest {
+
+    private LocationLPNValidatorForOutbound validatorForOutbound;
+
+    @BeforeEach
+    void setUp() {
+        validatorForOutbound = new LocationLPNValidatorForOutbound();
+    }
+
+    @Test
+    @DisplayName("출고 대상 LocationLPN 목록이 출고 수량을 충족하는지 검증한다. [예외발생]")
+    void validate_exception() {
+        assertValidate(
+                createInsufficientInventoryQuantityLocationLPNList(),
+                2,
+                IllegalArgumentException.class,
+                "출고 가능한 재고가 부족합니다. 출고 가능한 재고 수량: 1 출고 요청 수량: 2");
+
+        assertValidate(
+                List.of(Instancio.create(LocationLPN.class)),
+                0,
+                IllegalArgumentException.class,
+                "출고 요청 수량이 0보다 작거나 같습니다.");
+
+        assertValidate(
+                List.of(),
+                1,
+                IllegalArgumentException.class,
+                "출고 가능한지 검증할 로케이션 LPN 목록이 비어있습니다.");
+        assertValidate(
+                null,
+                1,
+                IllegalArgumentException.class,
+                "출고 가능한지 검증할 로케이션 LPN 목록이 비어있습니다.");
+    }
+
+    private void assertValidate(
+            final List<LocationLPN> locationLPNList,
+            final int orderQuantity,
+            final Class<?> exceptionClass,
+            final String errorMessage) {
+
+        assertThatThrownBy(() -> {
+            validatorForOutbound.validate(locationLPNList, orderQuantity);
+        }).isInstanceOf(exceptionClass)
+                .hasMessageContaining(errorMessage);
+    }
+
+    private List<LocationLPN> createInsufficientInventoryQuantityLocationLPNList() {
+        final LocationLPN locationLPN = Instancio.of(LocationLPN.class)
+                .supply(Select.field(LocationLPN::getInventoryQuantity), () -> 1)
+                .create();
+        return List.of(locationLPN);
+    }
+
+    @Test
+    @DisplayName("출고 대상 LocationLPN 목록이 출고 수량을 충족하는지 검증한다. [통과]")
+    void sufficient_inventory_location_lpn_list_for_outbound() {
+        final List<LocationLPN> locationLPNList = createSufficientInventoryQuantityLocationLPNList();
+        final int orderQuantity = 2;
+
+        validatorForOutbound.validate(locationLPNList, orderQuantity);
+    }
+
+    private List<LocationLPN> createSufficientInventoryQuantityLocationLPNList() {
+        final LocationLPN locationLPN = Instancio.of(LocationLPN.class)
+                .supply(Select.field(LocationLPN::getInventoryQuantity), () -> 2)
+                .create();
+        return List.of(locationLPN);
+    }
+}
