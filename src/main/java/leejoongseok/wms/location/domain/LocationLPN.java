48b9c70e82b7796a9ce57349aae948bed4ee0715
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-14 15:21:03 +0900

    import

--- src/main/java/leejoongseok/wms/location/domain/LocationLPN.java
+++ src/main/java/leejoongseok/wms/location/domain/LocationLPN.java
@@ -1,6 +1,14 @@
 package leejoongseok.wms.location.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import leejoongseok.wms.inbound.domain.LPN;
 import lombok.AccessLevel;
 import lombok.Getter;
--- src/main/java/leejoongseok/wms/outbound/domain/Outbound.java
+++ src/main/java/leejoongseok/wms/outbound/domain/Outbound.java
@@ -1,6 +1,17 @@
 package leejoongseok.wms.outbound.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
