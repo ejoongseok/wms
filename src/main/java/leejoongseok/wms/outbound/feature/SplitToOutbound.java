7449f2e09bd4b2be541837024d4a9f19a139783a
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-17 17:20:09 +0900

    add parameter

--- src/main/java/leejoongseok/wms/outbound/feature/SplitToOutbound.java
+++ src/main/java/leejoongseok/wms/outbound/feature/SplitToOutbound.java
@@ -1,5 +1,8 @@
 package leejoongseok.wms.outbound.feature;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import leejoongseok.wms.outbound.domain.Outbound;
 import leejoongseok.wms.outbound.domain.OutboundItemToSplit;
 import leejoongseok.wms.outbound.domain.OutboundRepository;
@@ -20,7 +23,13 @@ public class SplitToOutbound {
     }
 
     public record Request(
+            @NotNull(message = "분할 대상 완충재 수량은 필수 입니다.")
+            @Min(value = 0, message = "분할 대상 완충재 수량은 0보다 커야 합니다.")
+            Integer cushioningMaterialQuantity,
+            @NotNull(message = "분할 대상 출고 ID는 필수 입니다.")
             Long outBoundIdToSplit,
+            @NotNull(message = "분할 대상 출고 상품은 필수 입니다.")
+            @NotEmpty(message = "분할 대상 출고 상품은 비어 있을 수 없습니다.")
             List<Item> itemsToSplit
     ) {
         List<OutboundItemToSplit> listOfOutboundItemToSplit() {
@@ -32,7 +41,10 @@ public class SplitToOutbound {
         }
 
         public record Item(
+                @NotNull(message = "분할 대상 출고 상품 ID는 필수 입니다.")
                 Long outboundItemIdToSplit,
+                @NotNull(message = "분할 대상 출고 상품 수량은 필수 입니다.")
+                @Min(value = 0, message = "분할 대상 출고 상품 수량은 0보다 커야 합니다.")
                 Integer quantityOfSplit) {
         }
     }
--- src/test/java/leejoongseok/wms/outbound/feature/SplitToOutboundTest.java
+++ src/test/java/leejoongseok/wms/outbound/feature/SplitToOutboundTest.java
@@ -21,12 +21,14 @@ class SplitToOutboundTest {
     @Test
     @DisplayName("대기중인 출고건을 분할한다.")
     void splitToOutbound() {
+        final Integer cushioningMaterialQuantity = 1;
         final Long outBoundIdToSplit = 1L;
         final List<SplitToOutbound.Request.Item> itemsToSplit = List.of(
                 new SplitToOutbound.Request.Item(1L, 1),
                 new SplitToOutbound.Request.Item(2L, 1)
         );
         final SplitToOutbound.Request request = new SplitToOutbound.Request(
+                cushioningMaterialQuantity,
                 outBoundIdToSplit,
                 itemsToSplit
         );
