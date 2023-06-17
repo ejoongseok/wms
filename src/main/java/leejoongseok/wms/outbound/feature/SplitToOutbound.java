62671b532271a9f6ec04af8aa42c41e2a252c7a1
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-17 16:44:04 +0900

    add paramter

--- src/main/java/leejoongseok/wms/outbound/feature/SplitToOutbound.java
+++ src/main/java/leejoongseok/wms/outbound/feature/SplitToOutbound.java
@@ -1,10 +1,20 @@
 package leejoongseok.wms.outbound.feature;
 
+import java.util.List;
+
 public class SplitToOutbound {
     public void request(final Request request) {
 
     }
 
-    public record Request() {
+    public record Request(
+            Long outBoundIdToSplit,
+            List<Item> itemsToSplit
+    ) {
+
+        public record Item(
+                Long outboundItemIdToSplit,
+                int quantityOfSplit) {
+        }
     }
 }
--- src/test/java/leejoongseok/wms/outbound/feature/SplitToOutboundTest.java
+++ src/test/java/leejoongseok/wms/outbound/feature/SplitToOutboundTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class SplitToOutboundTest {
 
     private SplitToOutbound splitToOutbound;
@@ -16,7 +18,16 @@ class SplitToOutboundTest {
     @Test
     @DisplayName("대기중인 출고건을 분할한다.")
     void splitToOutbound() {
-        final SplitToOutbound.Request request = new SplitToOutbound.Request();
+        final Long outBoundIdToSplit = 1L;
+        final List<SplitToOutbound.Request.Item> itemsToSplit = List.of(
+                new SplitToOutbound.Request.Item(1L, 1),
+                new SplitToOutbound.Request.Item(2L, 1)
+        );
+        final SplitToOutbound.Request request = new SplitToOutbound.Request(
+                outBoundIdToSplit,
+                itemsToSplit
+        );
+
         splitToOutbound.request(request);
     }
 }
