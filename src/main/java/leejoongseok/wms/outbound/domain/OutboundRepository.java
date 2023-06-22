70599d1a09176a8d0c0f877b0593231234bbb0d4
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-22 21:31:15 +0900

    add library guava

--- src/main/java/leejoongseok/wms/outbound/domain/OutboundRepository.java
+++ src/main/java/leejoongseok/wms/outbound/domain/OutboundRepository.java
@@ -1,5 +1,6 @@
 package leejoongseok.wms.outbound.domain;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -7,6 +8,7 @@ import java.util.Optional;
 
 public interface OutboundRepository extends JpaRepository<Outbound, Long> {
 
+    @VisibleForTesting
     @Query("select o from Outbound o join fetch o.outboundItems oi where o.id = :outboundId")
     Optional<Outbound> testingFindById(Long outboundId);
 }
