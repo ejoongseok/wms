4eae6bc1304b8330efb0384b08936db072c41eca
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-26 06:01:25 +0900

    add comment

--- src/main/java/leejoongseok/wms/outbound/feature/AssignPickingTote.java
+++ src/main/java/leejoongseok/wms/outbound/feature/AssignPickingTote.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 출고에 피킹 토트를 할당하는 기능을 담당하는 핸들러
+ */
 @RestController
 @RequiredArgsConstructor
 public class AssignPickingTote {
