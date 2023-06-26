5a5f1cc9c5f013ead02e7272b4509df3b566f198
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-26 23:04:29 +0900

    중간커밋

--- src/main/java/leejoongseok/wms/outbound/feature/PickingAllocator.java
+++ src/main/java/leejoongseok/wms/outbound/feature/PickingAllocator.java
@@ -1,95 +1,60 @@
 package leejoongseok.wms.outbound.feature;
 
 import leejoongseok.wms.location.domain.LocationLPN;
+import leejoongseok.wms.outbound.domain.EfficiencyLocationLPNSorter;
 import leejoongseok.wms.outbound.domain.Outbound;
 import leejoongseok.wms.outbound.domain.OutboundItem;
-import leejoongseok.wms.outbound.exception.NotEnoughInventoryException;
+import leejoongseok.wms.outbound.domain.Picking;
+import leejoongseok.wms.outbound.domain.PickingAllocationValidator;
+import leejoongseok.wms.outbound.domain.PickingCreator;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-import java.util.Comparator;
 import java.util.List;
 
 @Component
 public class PickingAllocator {
+    private final EfficiencyLocationLPNSorter efficiencyLocationLPNSorter = new EfficiencyLocationLPNSorter();
+    private final PickingCreator pickingCreator = new PickingCreator();
+    private final PickingAllocationValidator pickingAllocationValidator = new PickingAllocationValidator();
+
+    /**
+     * 집품을 할당한다.
+     */
     public void allocate(
             final Outbound outbound,
             final List<LocationLPN> locationLPNList) {
         final List<OutboundItem> outboundItems = outbound.getOutboundItems();
-        final List<LocationLPN> pickingAllocatableLocationLPNList = filterByPickingAllocatable(
-                locationLPNList);
-        validatePickingAllocatable(
+        pickingAllocationValidator.validate(
                 outboundItems,
-                pickingAllocatableLocationLPNList);
+                locationLPNList);
 
         for (final OutboundItem outboundItem : outboundItems) {
-            final Long itemId = outboundItem.getItemId();
-            final List<LocationLPN> locationLPNS = getLocationLPNListByItemId(locationLPNList, itemId);
-
-            final List<LocationLPN> sortedLocationLPNList = sort(locationLPNS);
-        }
-
-    }
-
-    private List<LocationLPN> filterByPickingAllocatable(
-            final List<LocationLPN> locationLPNList) {
-        return locationLPNList.stream()
-                .filter(locationLPN -> locationLPN.isPickingAllocatable(
-                        LocalDateTime.now()))
-                .toList();
-    }
-
-    private void validatePickingAllocatable(
-            final List<OutboundItem> outboundItems,
-            final List<LocationLPN> locationLPNList) {
-        for (final OutboundItem outboundItem : outboundItems) {
-            final Long itemId = outboundItem.getItemId();
-            final long availableInventoryQuantity = calculateAvailableInventoryQuantity(
+            final List<LocationLPN> sortedLocationLPNList = filterByItemIdAndSortLocationLPNList(
                     locationLPNList,
-                    itemId);
-            if (!isEnoughInventoryQuantity(
+                    outboundItem.getItemId());
+
+            final List<Picking> pickings = pickingCreator.createPickings(
+                    outboundItem.getItemId(),
                     outboundItem.getOutboundQuantity(),
-                    availableInventoryQuantity)) {
-                throw new NotEnoughInventoryException("집품할 상품의 재고가 부족합니다." +
-                        "상품ID: %d, 재고수량: %d, 필요한 수량: %d".formatted(
-                                itemId,
-                                availableInventoryQuantity,
-                                outboundItem.getOutboundQuantity()));
-            }
+                    sortedLocationLPNList
+            );
+
+            outboundItem.assignPickings(pickings);
         }
-    }
 
-    private long calculateAvailableInventoryQuantity(
-            final List<LocationLPN> locationLPNList,
-            final Long itemId) {
-        return locationLPNList.stream()
-                .filter(locationLPN -> locationLPN.getItemId().equals(itemId))
-                .mapToLong(locationLPN -> locationLPN.getInventoryQuantity())
-                .sum();
-    }
-
-    private boolean isEnoughInventoryQuantity(
-            final Integer outboundQuantity,
-            final Long availableInventoryQuantity) {
-        return outboundQuantity <= availableInventoryQuantity;
-    }
-
-    private List<LocationLPN> getLocationLPNListByItemId(final List<LocationLPN> locationLPNList, final Long itemId) {
-        return locationLPNList.stream()
-                .filter(locationLPN -> locationLPN.getItemId().equals(itemId))
-                .toList();
     }
 
     /**
-     * 1. 유통기한이 가장 빠른 순서
-     * 2. 재고수량이 많은 순서
-     * 3. 로케이션 바코드 순서 A -> Z
+     * 상품ID에 해당하는 LocationLPN을 집품하기에 효율적으로 정렬후 반환한다.
      */
-    private List<LocationLPN> sort(final List<LocationLPN> locationLPNS) {
-        return locationLPNS.stream()
-                .sorted(Comparator.comparing(LocationLPN::getExpirationAt)
-                        .thenComparing(LocationLPN::getInventoryQuantity).reversed()
-                        .thenComparing(LocationLPN::getLocationBarcode))
+    private List<LocationLPN> filterByItemIdAndSortLocationLPNList(
+            final List<LocationLPN> locationLPNList,
+            final Long itemId) {
+        final List<LocationLPN> locationLPNS = locationLPNList.stream()
+                .filter(locationLPN -> locationLPN.getItemId().equals(itemId))
                 .toList();
+        return efficiencyLocationLPNSorter.sort(
+                locationLPNS);
     }
+
 }
