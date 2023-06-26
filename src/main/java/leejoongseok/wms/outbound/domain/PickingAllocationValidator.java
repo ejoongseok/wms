7427c66a2cfd46e55a75c5edc931ac9a4287408d
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-26 22:22:11 +0900

    create PickingAllocationValidator

---
+++ src/main/java/leejoongseok/wms/outbound/domain/PickingAllocationValidator.java
@@ -0,0 +1,50 @@
+package leejoongseok.wms.outbound.domain;
+
+import leejoongseok.wms.location.domain.LocationLPN;
+import leejoongseok.wms.outbound.exception.NotEnoughInventoryException;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 출고하려는 주문이 집품 가능한지 확인.
+ */
+public class PickingAllocationValidator {
+    public void validate(
+            final List<OutboundItem> outboundItems,
+            final List<LocationLPN> locationLPNList) {
+        for (final OutboundItem outboundItem : outboundItems) {
+            final long availableInventoryQuantity = calculateAvailableInventoryQuantity(
+                    locationLPNList,
+                    outboundItem.getItemId());
+
+            if (isEnoughInventoryQuantity(outboundItem.getOutboundQuantity(), availableInventoryQuantity)) {
+                continue;
+            }
+
+            throw new NotEnoughInventoryException("집품할 상품의 재고가 부족합니다." +
+                    "상품ID: %d, 재고수량: %d, 필요한 수량: %d".formatted(
+                            outboundItem.getItemId(),
+                            availableInventoryQuantity,
+                            outboundItem.getOutboundQuantity()));
+        }
+    }
+
+    private long calculateAvailableInventoryQuantity(
+            final List<LocationLPN> locationLPNList,
+            final Long itemId) {
+        return locationLPNList.stream()
+                .filter(locationLPN -> locationLPN.getItemId().equals(itemId))
+                .filter(locationLPN -> locationLPN.isPickingAllocatable(
+                        LocalDateTime.now()))
+                .mapToLong(locationLPN -> locationLPN.getInventoryQuantity())
+                .sum();
+    }
+
+    private boolean isEnoughInventoryQuantity(
+            final Integer outboundQuantity,
+            final Long availableInventoryQuantity) {
+        return outboundQuantity <= availableInventoryQuantity;
+    }
+
+}
