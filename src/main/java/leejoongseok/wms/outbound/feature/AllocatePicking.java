8c777446fe5a8a1f1a1d226b0d809bf7873b78e9
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-27 23:36:01 +0900

    create method request

--- src/main/java/leejoongseok/wms/outbound/feature/AllocatePicking.java
+++ src/main/java/leejoongseok/wms/outbound/feature/AllocatePicking.java
@@ -1,4 +1,7 @@
 package leejoongseok.wms.outbound.feature;
 
 public class AllocatePicking {
+    public void request() {
+
+    }
 }
--- src/test/java/leejoongseok/wms/outbound/feature/AllocatePickingTest.java
+++ src/test/java/leejoongseok/wms/outbound/feature/AllocatePickingTest.java
@@ -17,6 +17,6 @@ public class AllocatePickingTest {
     @Test
     @DisplayName("출고 상품에 대한 집품 목록을 할당한다.")
     void allocatePicking() {
-
+        allocatePicking.request();
     }
 }
