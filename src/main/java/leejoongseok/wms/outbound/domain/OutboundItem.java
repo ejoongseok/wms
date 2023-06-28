c0246b6fb2d303516ea9814ec191e00d0da0b385
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-28 22:51:00 +0900

    create method isCompletedPicking

--- src/main/java/leejoongseok/wms/outbound/domain/OutboundItem.java
+++ src/main/java/leejoongseok/wms/outbound/domain/OutboundItem.java
@@ -163,4 +163,8 @@ public class OutboundItem {
         }
     }
 
+    public boolean isCompletedPicking() {
+        return pickings.stream()
+                .allMatch(Picking::isCompletedPicking);
+    }
 }
