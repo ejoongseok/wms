48acecf73a12823c85afab83674f45244cef5ef5
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-28 22:41:38 +0900

    create CompletePicking

---
+++ src/main/java/leejoongseok/wms/outbound/feature/CompletePicking.java
@@ -0,0 +1,9 @@
+package leejoongseok.wms.outbound.feature;
+
+/**
+ * 출고해야할 상품의 집품이 모두 완료되면 집품완료를 할 수 있다.
+ */
+public class CompletePicking {
+
+
+}
---
+++ src/test/java/leejoongseok/wms/outbound/feature/CompletePickingTest.java
@@ -0,0 +1,13 @@
+package leejoongseok.wms.outbound.feature;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CompletePickingTest {
+
+    @Test
+    @DisplayName("출고해야할 상품의 집품이 모두 완료되면 집품완료를 할 수 있다.")
+    void completePicking() {
+
+    }
+}
