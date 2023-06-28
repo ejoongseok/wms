d6b3495987454acc147c1087a088d26d98a63b26
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-28 22:08:20 +0900

    create Request

--- src/main/java/leejoongseok/wms/outbound/feature/ManualToPick.java
+++ src/main/java/leejoongseok/wms/outbound/feature/ManualToPick.java
@@ -1,8 +1,24 @@
 package leejoongseok.wms.outbound.feature;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 /**
  * 집품해야할 로케이션에 가서 LPN을 선택하고 토트에 담을 상품의 수량을 입력합니다.
  * 집품해야할 상품이 많을 경우 스캔해서 집품 수량을 올리는게 아닌 한번에 토트에 담아두고 직접 수량을 입력해서 집품을 진행합니다.
  */
 public class ManualToPick {
+    public void request(final Request request) {
+        throw new UnsupportedOperationException("Unsupported request");
+    }
+
+    public record Request(
+            @NotNull(message = "피킹 ID는 필수 입니다.")
+            Long pickingId,
+            @NotNull(message = "로케이션 LPN ID는 필수 입니다.")
+            Long locationLPNId,
+            @NotNull(message = "집품할 수량은 필수 입니다.")
+            @Min(value = 1, message = "집품할 수량은 1개 이상이어야 합니다.")
+            Integer pickedQuantity) {
+    }
 }
--- src/test/java/leejoongseok/wms/outbound/feature/ManualToPickTest.java
+++ src/test/java/leejoongseok/wms/outbound/feature/ManualToPickTest.java
@@ -1,11 +1,30 @@
 package leejoongseok.wms.outbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ManualToPickTest {
 
+    private ManualToPick manualToPick;
+
+    @BeforeEach
+    void setUp() {
+        manualToPick = new ManualToPick();
+    }
+
     @Test
     void manualToPick() {
+        final Long pickingId = 1L;
+        final Long locationLPNId = 1L;
+        final Integer pickedQuantity = 10;
+        final ManualToPick.Request request = new ManualToPick.Request(
+                pickingId,
+                locationLPNId,
+                pickedQuantity
+        );
+
+        manualToPick.request(request);
+
 
     }
 }
