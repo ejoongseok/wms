1701eb16338a38ff4d8de8fd48470006d62f42dd
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-28 22:43:10 +0900

    create request

--- src/main/java/leejoongseok/wms/outbound/feature/CompletePicking.java
+++ src/main/java/leejoongseok/wms/outbound/feature/CompletePicking.java
@@ -1,9 +1,19 @@
 package leejoongseok.wms.outbound.feature;
 
+import jakarta.validation.constraints.NotNull;
+
 /**
  * 출고해야할 상품의 집품이 모두 완료되면 집품완료를 할 수 있다.
  */
 public class CompletePicking {
 
 
+    public void request(final Request request) {
+
+    }
+
+    public record Request(
+            @NotNull(message = "집품을 완료하려는 출고 ID는 필수 입니다.")
+            Long outboundId) {
+    }
 }
--- src/test/java/leejoongseok/wms/outbound/feature/CompletePickingTest.java
+++ src/test/java/leejoongseok/wms/outbound/feature/CompletePickingTest.java
@@ -1,13 +1,28 @@
 package leejoongseok.wms.outbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CompletePickingTest {
 
+    private CompletePicking completePicking;
+
+    @BeforeEach
+    void setUp() {
+        completePicking = new CompletePicking();
+    }
+
     @Test
     @DisplayName("출고해야할 상품의 집품이 모두 완료되면 집품완료를 할 수 있다.")
     void completePicking() {
+        final Long outboundId = 1L;
+        final CompletePicking.Request request = new CompletePicking.Request(
+                outboundId
+        );
 
+        completePicking.request(request);
+
+        // outboundRepository.findById(1L).get().getStatus() == PickingCompleted
     }
 }
