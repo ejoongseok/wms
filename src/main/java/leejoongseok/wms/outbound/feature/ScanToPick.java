e573a1cf0edfd4adbe5ed6583fa9f5fbdf961c39
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-28 17:11:32 +0900

    create method request

--- src/main/java/leejoongseok/wms/outbound/feature/ScanToPick.java
+++ src/main/java/leejoongseok/wms/outbound/feature/ScanToPick.java
@@ -2,4 +2,7 @@ package leejoongseok.wms.outbound.feature;
 
 public class ScanToPick {
 
+    public void request() {
+        throw new UnsupportedOperationException("Unsupported request");
+    }
 }
--- src/test/java/leejoongseok/wms/outbound/feature/ScanToPickTest.java
+++ src/test/java/leejoongseok/wms/outbound/feature/ScanToPickTest.java
@@ -1,13 +1,21 @@
 package leejoongseok.wms.outbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ScanToPickTest {
 
+    private ScanToPick scanToPick;
+
+    @BeforeEach
+    void setUp() {
+        scanToPick = new ScanToPick();
+    }
+
     @Test
     @DisplayName("집품정보를 확인한 뒤 집품할 장소에가서 LocationBarcode와 상품의 LPNBarcode를 스캔해서 집품한다.")
     void scanToPick() {
-
+        scanToPick.request();
     }
 }
