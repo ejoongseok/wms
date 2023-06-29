355a65ef352cda4a2130af7f11c622dc82cb569f
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-30 07:02:35 +0900

    add request

--- src/main/java/leejoongseok/wms/outbound/feature/CompletePacking.java
+++ src/main/java/leejoongseok/wms/outbound/feature/CompletePacking.java
@@ -1,4 +1,7 @@
 package leejoongseok.wms.outbound.feature;
 
 public class CompletePacking {
+    public void request(final Long outboundId) {
+        throw new UnsupportedOperationException("Unsupported request");
+    }
 }
--- src/test/java/leejoongseok/wms/outbound/feature/CompletePackingTest.java
+++ src/test/java/leejoongseok/wms/outbound/feature/CompletePackingTest.java
@@ -1,12 +1,22 @@
 package leejoongseok.wms.outbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CompletePackingTest {
+
+    private CompletePacking completePacking;
+
+    @BeforeEach
+    void setUp() {
+        completePacking = new CompletePacking();
+    }
+
     @Test
     @DisplayName("출고에대한 포장을 완료한다.")
     void completePacking() {
-
+        final Long outboundId = 1L;
+        completePacking.request(outboundId);
     }
 }
