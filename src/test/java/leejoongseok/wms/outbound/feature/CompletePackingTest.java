1dfa6863bd7bfdbc07d292be92cfc8c06ed41a73
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-30 07:02:06 +0900

    create CompletePacking

---
+++ src/test/java/leejoongseok/wms/outbound/feature/CompletePackingTest.java
@@ -0,0 +1,12 @@
+package leejoongseok.wms.outbound.feature;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CompletePackingTest {
+    @Test
+    @DisplayName("출고에대한 포장을 완료한다.")
+    void completePacking() {
+
+    }
+}
