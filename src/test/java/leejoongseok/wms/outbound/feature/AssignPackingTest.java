695bab66bee41e865fa2c6e1287f7f8515458b24
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-06-30 06:06:02 +0900

    create assignPacking

---
+++ src/test/java/leejoongseok/wms/outbound/feature/AssignPackingTest.java
@@ -0,0 +1,21 @@
+package leejoongseok.wms.outbound.feature;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AssignPackingTest {
+
+    private AssignPacking assignPacking;
+
+    @BeforeEach
+    void setUp() {
+        assignPacking = new AssignPacking();
+    }
+
+    @Test
+    @DisplayName("패킹 정보를 등록한다.")
+    void assignPacking() {
+
+    }
+}
