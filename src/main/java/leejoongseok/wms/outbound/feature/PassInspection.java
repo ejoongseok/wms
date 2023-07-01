8c99101503fe2d1a864bcc243a99c80053a681e5
이중석 <99948743+ejoongseok@users.noreply.github.com>
2023-07-01 15:41:49 +0900

    WMS-23 | 집품한 출고상품의 검수를 통과한다. (#23)
    * create PassInspection
    
    * create PassInspection
    
    * create component
    
    * change component -> api
    
    ---------
    
    Co-authored-by: 이중석/백엔드개발팀 <jslee@ktown4u.com>

---
+++ src/main/java/leejoongseok/wms/outbound/feature/PassInspection.java
@@ -0,0 +1,31 @@
+package leejoongseok.wms.outbound.feature;
+
+import leejoongseok.wms.outbound.domain.Outbound;
+import leejoongseok.wms.outbound.domain.OutboundRepository;
+import leejoongseok.wms.outbound.exception.OutboundIdNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 집품한 상품이 출고해야하는 상품이 맞는지, 파손이나 누락이 없는지 검사에 통과한다.
+ */
+@RestController
+@RequiredArgsConstructor
+public class PassInspection {
+    private final OutboundRepository outboundRepository;
+
+    @Transactional
+    @PostMapping("/outbounds/{outboundId}/pass-inspection")
+    public void request(@PathVariable final Long outboundId) {
+        final Outbound outbound = getOutbound(outboundId);
+        outbound.passInspection();
+    }
+
+    private Outbound getOutbound(final Long outboundId) {
+        return outboundRepository.findById(outboundId)
+                .orElseThrow(() -> new OutboundIdNotFoundException(outboundId));
+    }
+}
