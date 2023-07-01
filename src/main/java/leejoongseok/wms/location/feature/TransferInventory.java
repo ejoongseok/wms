efc0eee83fb220c4442a9f66442f3d5949d9d959
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-01 11:42:36 +0900

    create TransferInventory

---
+++ src/main/java/leejoongseok/wms/location/feature/TransferInventory.java
@@ -0,0 +1,8 @@
+package leejoongseok.wms.location.feature;
+
+/**
+ * A 로케이션에 재고 10개 중 5개를 B 로케이션으로 이동시키는 기능
+ * ex) 진열구역에 집품해야할 재고를 보충하기 위해 보충구역에서 재고 일부를 진열구역으로 이동시킨다.
+ */
+public class TransferInventory {
+}
---
+++ src/test/java/leejoongseok/wms/location/feature/TransferInventoryTest.java
@@ -0,0 +1,13 @@
+package leejoongseok.wms.location.feature;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TransferInventoryTest {
+
+    @Test
+    @DisplayName("A 로케이션에 재고 10개 중 5개를 B 로케이션으로 이동시키는 기능")
+    void transferInventory() {
+
+    }
+}
