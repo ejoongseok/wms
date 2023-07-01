da9df1cf8af2cff73afd1c2577a0a6b86996b1f1
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-01 09:56:19 +0900

    create MoveToTargetChildLocation

---
+++ src/test/java/leejoongseok/wms/location/feature/MoveToTargetChildLocationTest.java
@@ -0,0 +1,20 @@
+package leejoongseok.wms.location.feature;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MoveToTargetChildLocationTest {
+
+    private MoveToTargetChildLocation moveToTargetChildLocation;
+
+    @BeforeEach
+    void setUp() {
+        moveToTargetChildLocation = new MoveToTargetChildLocation();
+    }
+
+    @Test
+    @DisplayName("대상 로케이션의 하위 로케이션으로 현재 로케이션을 이동한다.")
+    void moveToTargetChildLocation() {
+    }
+}
