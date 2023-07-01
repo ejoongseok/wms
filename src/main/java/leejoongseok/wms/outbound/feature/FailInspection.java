9a56d67c2b85465978a743d3d53c85dd037be0b6
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-01 15:51:10 +0900

    create component

--- src/main/java/leejoongseok/wms/outbound/feature/FailInspection.java
+++ src/main/java/leejoongseok/wms/outbound/feature/FailInspection.java
@@ -6,15 +6,19 @@ import leejoongseok.wms.outbound.domain.Outbound;
 import leejoongseok.wms.outbound.domain.OutboundRepository;
 import leejoongseok.wms.outbound.exception.OutboundIdNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 집품한 출고상품의 검수가 어떠한 이유로 통과하지 못한다.
  */
+@Component
 @RequiredArgsConstructor
 public class FailInspection {
     private final OutboundRepository outboundRepository;
 
 
+    @Transactional
     public void request(final Request request) {
         final Outbound outbound = getOutbound(request);
 
--- src/test/java/leejoongseok/wms/outbound/feature/FailInspectionTest.java
+++ src/test/java/leejoongseok/wms/outbound/feature/FailInspectionTest.java
@@ -1,28 +1,55 @@
 package leejoongseok.wms.outbound.feature;
 
+import leejoongseok.wms.common.ApiTest;
+import leejoongseok.wms.common.Scenario;
+import leejoongseok.wms.location.domain.StorageType;
+import leejoongseok.wms.location.domain.UsagePurpose;
 import leejoongseok.wms.outbound.domain.OutboundRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-class FailInspectionTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+class FailInspectionTest extends ApiTest {
+
+    @Autowired
     private FailInspection failInspection;
+    @Autowired
     private OutboundRepository outboundRepository;
 
-    @BeforeEach
-    void setUp() {
-        outboundRepository = null;
-        failInspection = new FailInspection(outboundRepository);
-    }
 
     @Test
     @DisplayName("출고 검수 불합격")
     void failInspection() {
+        new Scenario()
+                .createItem().request()
+                .createInbound().request()
+                .confirmInspectedInbound().request()
+                .createLPN().request()
+                .createLocation().request()
+                .assignLPNToLocation().request(3)
+                .createPackagingMaterial().request()
+                .createOutbound().request();
+
+        final String toteBarcode = "TOTE0001";
+        new Scenario()
+                .createLocation()
+                .locationBarcode(toteBarcode)
+                .storageType(StorageType.TOTE)
+                .usagePurpose(UsagePurpose.MOVE)
+                .request()
+                .assignPickingTote().request()
+                .allocatePicking().request()
+                .manualToPick().pickedQuantity(2).request()
+                .completePicking().request();
+
         final Long outboundId = 1L;
         final String stoppedReason = "품질 불량";
         final FailInspection.Request request = new FailInspection.Request(outboundId, stoppedReason);
 
         failInspection.request(request);
+
+        assertThat(outboundRepository.findById(1L).get().isStopped()).isTrue();
     }
 }
