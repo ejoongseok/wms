fe219a7c33fad1c22768f41940002438f1448caf
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-01 15:38:48 +0900

    create component

--- src/main/java/leejoongseok/wms/outbound/feature/PassInspection.java
+++ src/main/java/leejoongseok/wms/outbound/feature/PassInspection.java
@@ -4,14 +4,18 @@ import leejoongseok.wms.outbound.domain.Outbound;
 import leejoongseok.wms.outbound.domain.OutboundRepository;
 import leejoongseok.wms.outbound.exception.OutboundIdNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 집품한 상품이 출고해야하는 상품이 맞는지, 파손이나 누락이 없는지 검사에 통과한다.
  */
+@Component
 @RequiredArgsConstructor
 public class PassInspection {
     private final OutboundRepository outboundRepository;
 
+    @Transactional
     public void request(final Long outboundId) {
         final Outbound outbound = getOutbound(outboundId);
         outbound.passInspection();
--- src/test/java/leejoongseok/wms/outbound/feature/PassInspectionTest.java
+++ src/test/java/leejoongseok/wms/outbound/feature/PassInspectionTest.java
@@ -1,25 +1,52 @@
 package leejoongseok.wms.outbound.feature;
 
+import leejoongseok.wms.common.ApiTest;
+import leejoongseok.wms.common.Scenario;
+import leejoongseok.wms.location.domain.StorageType;
+import leejoongseok.wms.location.domain.UsagePurpose;
 import leejoongseok.wms.outbound.domain.OutboundRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-class PassInspectionTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+class PassInspectionTest extends ApiTest {
+
+    @Autowired
     private PassInspection passInspection;
+    @Autowired
     private OutboundRepository outboundRepository;
 
-    @BeforeEach
-    void setUp() {
-        outboundRepository = null;
-        passInspection = new PassInspection(outboundRepository);
-    }
 
     @Test
     @DisplayName("집품 완료한 출고건의 검수를 통과한다.")
     void passInspection() {
+        new Scenario()
+                .createItem().request()
+                .createInbound().request()
+                .confirmInspectedInbound().request()
+                .createLPN().request()
+                .createLocation().request()
+                .assignLPNToLocation().request(3)
+                .createPackagingMaterial().request()
+                .createOutbound().request();
+
+        final String toteBarcode = "TOTE0001";
+        new Scenario()
+                .createLocation()
+                .locationBarcode(toteBarcode)
+                .storageType(StorageType.TOTE)
+                .usagePurpose(UsagePurpose.MOVE)
+                .request()
+                .assignPickingTote().request()
+                .allocatePicking().request()
+                .manualToPick().pickedQuantity(2).request()
+                .completePicking().request();
         final Long outboundId = 1L;
+
         passInspection.request(outboundId);
+
+        assertThat(outboundRepository.findById(outboundId).get().isPassedInspection()).isTrue();
     }
 }
