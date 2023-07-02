fd6255095557ac47563015208cf983808abe3239
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-02 18:19:37 +0900

    create component

--- src/test/java/leejoongseok/wms/outbound/feature/StopOutboundTest.java
+++ src/test/java/leejoongseok/wms/outbound/feature/StopOutboundTest.java
@@ -1,29 +1,45 @@
 package leejoongseok.wms.outbound.feature;
 
+import leejoongseok.wms.common.ApiTest;
+import leejoongseok.wms.common.Scenario;
+import leejoongseok.wms.outbound.domain.Outbound;
 import leejoongseok.wms.outbound.domain.OutboundRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-class StopOutboundTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+class StopOutboundTest extends ApiTest {
+
+    @Autowired
     private StopOutbound stopOutbound;
+    @Autowired
     private OutboundRepository outboundRepository;
-
-    @BeforeEach
-    void setUp() {
-        outboundRepository = null;
-        stopOutbound = new StopOutbound(outboundRepository);
-    }
 
     @Test
     @DisplayName("어떠한 이유로 출고를 중지한다.")
     void stopOutbound() {
+        new Scenario()
+                .createItem().request()
+                .createInbound().request()
+                .confirmInspectedInbound().request()
+                .createLPN().request()
+                .createLocation().request()
+                .assignLPNToLocation().request(2)
+                .createPackagingMaterial().request()
+                .createOutbound().request();
+
         final Long outboundId = 1L;
         final String stoppedReason = "오집품/상품불량/포장손상..";
         final StopOutbound.Request request = new StopOutbound.Request(
                 stoppedReason
         );
+
         stopOutbound.request(outboundId, request);
+
+        final Outbound outbound = outboundRepository.findById(1L).get();
+        assertThat(outbound.isStopped()).isTrue();
+        assertThat(outbound.getStoppedReason()).isEqualTo(stoppedReason);
     }
 }
