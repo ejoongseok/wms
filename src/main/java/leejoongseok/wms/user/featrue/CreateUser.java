3456f7a494c3495106f7a909e1ee514f39282c82
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-02 22:25:40 +0900

    create user entity

--- src/main/java/leejoongseok/wms/user/featrue/CreateUser.java
+++ src/main/java/leejoongseok/wms/user/featrue/CreateUser.java
@@ -1,14 +1,26 @@
 package leejoongseok.wms.user.featrue;
 
 import jakarta.validation.constraints.NotBlank;
+import leejoongseok.wms.user.domain.User;
+import leejoongseok.wms.user.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
 
+
+@RequiredArgsConstructor
 public class CreateUser {
+    private final UserRepository userRepository;
+
     public void request(final Request request) {
-        throw new UnsupportedOperationException("Unsupported request");
+        final User user = request.toEntity();
+        userRepository.save(user);
     }
 
     public record Request(
             @NotBlank(message = "사용자 이름은 필수입니다.")
             String name) {
+
+        User toEntity() {
+            return new User(name);
+        }
     }
 }
