fad1ef7098af1dcd3f6a03c6d9e801c233500595
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-02 18:40:36 +0900

    create AdjustInventory

---
+++ src/main/java/leejoongseok/wms/location/feature/AdjustInventory.java
@@ -0,0 +1,9 @@
+package leejoongseok.wms.location.feature;
+
+/**
+ * 재고를 조정한다.
+ */
+public class AdjustInventory {
+
+
+}
---
+++ src/test/java/leejoongseok/wms/location/feature/AdjustInventoryTest.java
@@ -0,0 +1,22 @@
+package leejoongseok.wms.location.feature;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AdjustInventoryTest {
+
+
+    private AdjustInventory adjustInventory;
+
+    @BeforeEach
+    void setUp() {
+        adjustInventory = new AdjustInventory();
+    }
+
+    @Test
+    @DisplayName("재고를 조정한다.")
+    void adjustInventory() {
+
+    }
+}
