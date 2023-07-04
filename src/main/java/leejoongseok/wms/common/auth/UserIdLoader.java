6539c4e0ca5fb41b6173efb8e3f3e3a29083563b
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-04 22:29:36 +0900

    UserIdLoader SecurityContext에서 userId 가져오도록 변경

--- src/main/java/leejoongseok/wms/common/auth/UserIdLoader.java
+++ src/main/java/leejoongseok/wms/common/auth/UserIdLoader.java
@@ -1,10 +1,14 @@
 package leejoongseok.wms.common.auth;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserIdLoader {
     public Long getUserId() {
-        return 1L;
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        final UserIdToken userIdToken = (UserIdToken) authentication.getPrincipal();
+        return userIdToken.getUserId();
     }
 }
