047d85453e1dfff6ad645ef88e8d6e5becd31419
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-04 21:30:27 +0900

    create BaseEntity

---
+++ src/main/java/leejoongseok/wms/common/user/BaseEntity.java
@@ -0,0 +1,32 @@
+package leejoongseok.wms.common.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseEntity {
+    @CreatedBy
+    @Column(name = "created_user_id", nullable = false, updatable = false)
+    private Long createdUserId;
+
+    @LastModifiedBy
+    @Column(name = "updated_user_id", nullable = false)
+    private Long updatedUserId;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
