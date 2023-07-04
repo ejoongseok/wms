c734ab8aab42d61a2a01fd9c2e8a110dfce37884
이중석 <99948743+ejoongseok@users.noreply.github.com>
2023-07-04 22:30:23 +0900

    WMS-33 | spring security 적용 (#33)
    * add spring security
    
    * create SecurityConfig
    
    * add UserIdTokenProvider
    
    * AuthenticationFilter 구현
    
    * UserIdLoader SecurityContext에서 userId 가져오도록 변경
    
    ---------
    
    Co-authored-by: 이중석/백엔드개발팀 <jslee@ktown4u.com>

---
+++ src/main/java/leejoongseok/wms/common/auth/UserIdTokenProvider.java
@@ -0,0 +1,11 @@
+package leejoongseok.wms.common.auth;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserIdTokenProvider {
+    public UserIdToken parseToken(final String authorization) {
+        // TODO : parse token
+        return UserIdToken.of(1L);
+    }
+}
