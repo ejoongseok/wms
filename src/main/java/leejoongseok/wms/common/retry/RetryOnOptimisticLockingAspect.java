f1501644b0b0f2b86f6b3b5e5c5334da865cfade
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-07 17:21:42 +0900

    add comment

--- src/main/java/leejoongseok/wms/common/retry/RetryOnOptimisticLockingAspect.java
+++ src/main/java/leejoongseok/wms/common/retry/RetryOnOptimisticLockingAspect.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Component;
 
 /**
  * 재고 변경같은 정합성 유지가 필요한 작업에서 OptimisticLockException이 발생하면 재시도하는 Aspect
+ * spring retry를 직접 구현한 기능 https://www.baeldung.com/spring-retry
+ * 아직 retry를 사용할만한곳이 많지 않다고 판단되서 직접구현
  */
 @Slf4j
 @Aspect
