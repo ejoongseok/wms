36b6d472092f6fb6786a64cf55aa857e9d19e4da
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-17 22:08:03 +0900

    extract method

--- src/main/java/leejoongseok/wms/outbound/domain/LocationLPNList.java
+++ src/main/java/leejoongseok/wms/outbound/domain/LocationLPNList.java
@@ -6,10 +6,15 @@ import java.util.List;
 
 public record LocationLPNList(List<LocationLPN> locationLPNList) {
     static List<LocationLPN> getEfficiencyLocationLPNList(final LocationLPNList locationLPNList, final Long itemId) {
+        final List<LocationLPN> locationLPNS = listFrom(locationLPNList, itemId);
+        return EfficiencyLocationLPNSorter.sort(
+                locationLPNS);
+    }
+
+    private static List<LocationLPN> listFrom(final LocationLPNList locationLPNList, final Long itemId) {
         final List<LocationLPN> locationLPNS = locationLPNList.locationLPNList().stream()
                 .filter(locationLPN -> locationLPN.getItemId().equals(itemId))
                 .toList();
-        return EfficiencyLocationLPNSorter.sort(
-                locationLPNS);
+        return locationLPNS;
     }
 }
