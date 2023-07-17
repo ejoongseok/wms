8d721a659d2bed02616441579db3ee18128d070d
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-17 22:11:40 +0900

    assert constructor

--- src/main/java/leejoongseok/wms/outbound/domain/LocationLPNList.java
+++ src/main/java/leejoongseok/wms/outbound/domain/LocationLPNList.java
@@ -1,6 +1,7 @@
 package leejoongseok.wms.outbound.domain;
 
 import leejoongseok.wms.location.domain.LocationLPN;
+import org.springframework.util.Assert;
 
 import java.util.List;
 
@@ -8,6 +9,7 @@ public final class LocationLPNList {
     private final List<LocationLPN> locationLPNList;
 
     public LocationLPNList(final List<LocationLPN> locationLPNList) {
+        Assert.notEmpty(locationLPNList, "LocationLPN 목록이 존재하지 않습니다.");
         this.locationLPNList = locationLPNList;
     }
 
