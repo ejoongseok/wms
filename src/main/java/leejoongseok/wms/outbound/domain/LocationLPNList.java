c32baac9701b700b22b6595ab21cc81da8d80ed4
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-17 22:17:17 +0900

    make private

--- src/main/java/leejoongseok/wms/outbound/domain/LocationLPNList.java
+++ src/main/java/leejoongseok/wms/outbound/domain/LocationLPNList.java
@@ -32,7 +32,7 @@ public final class LocationLPNList {
      * 2. 재고수량이 많은 순서 (그래야 최대한 적은 로케이션 LPN으로 집품이 가능하니까)
      * 3. 로케이션 바코드 명으로 정렬 (보통 로케이션 바코드는 A -> Z 순서로 정렬되어 있음)
      */
-    public static List<LocationLPN> sort(final List<LocationLPN> locationLPNList) {
+    private static List<LocationLPN> sort(final List<LocationLPN> locationLPNList) {
         validateParameter(locationLPNList);
         return locationLPNList.stream()
                 .sorted(Comparator.comparing(LocationLPN::getExpirationAt).reversed()
