aadf7d0423785fb0dff074f67d90459872ab18e7
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-17 22:07:11 +0900

    move member

--- src/main/java/leejoongseok/wms/outbound/domain/LocationLPNList.java
+++ src/main/java/leejoongseok/wms/outbound/domain/LocationLPNList.java
@@ -5,4 +5,11 @@ import leejoongseok.wms.location.domain.LocationLPN;
 import java.util.List;
 
 public record LocationLPNList(List<LocationLPN> locationLPNList) {
+    static List<LocationLPN> getEfficiencyLocationLPNList(final LocationLPNList locationLPNList, final Long itemId) {
+        final List<LocationLPN> locationLPNS = locationLPNList.locationLPNList().stream()
+                .filter(locationLPN -> locationLPN.getItemId().equals(itemId))
+                .toList();
+        return EfficiencyLocationLPNSorter.sort(
+                locationLPNS);
+    }
 }
--- src/main/java/leejoongseok/wms/outbound/domain/PickingAllocator.java
+++ src/main/java/leejoongseok/wms/outbound/domain/PickingAllocator.java
@@ -39,15 +39,7 @@ public enum PickingAllocator {
     private static List<LocationLPN> filterByItemIdAndSortLocationLPNList(
             final LocationLPNList locationLPNList,
             final Long itemId) {
-        return getEfficiencyLocationLPNList(locationLPNList, itemId);
-    }
-
-    private static List<LocationLPN> getEfficiencyLocationLPNList(final LocationLPNList locationLPNList, final Long itemId) {
-        final List<LocationLPN> locationLPNS = locationLPNList.locationLPNList().stream()
-                .filter(locationLPN -> locationLPN.getItemId().equals(itemId))
-                .toList();
-        return EfficiencyLocationLPNSorter.sort(
-                locationLPNS);
+        return LocationLPNList.getEfficiencyLocationLPNList(locationLPNList, itemId);
     }
 
 }
