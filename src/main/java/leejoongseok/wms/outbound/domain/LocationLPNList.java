14c962468a7b9a6c6a71be74e7db38e9f90db524
이중석/백엔드개발팀 <jslee@ktown4u.com>
2023-07-17 22:09:18 +0900

    convert to class

--- src/main/java/leejoongseok/wms/outbound/domain/LocationLPNList.java
+++ src/main/java/leejoongseok/wms/outbound/domain/LocationLPNList.java
@@ -3,8 +3,15 @@ package leejoongseok.wms.outbound.domain;
 import leejoongseok.wms.location.domain.LocationLPN;
 
 import java.util.List;
+import java.util.Objects;
 
-public record LocationLPNList(List<LocationLPN> locationLPNList) {
+public final class LocationLPNList {
+    private final List<LocationLPN> locationLPNList;
+
+    public LocationLPNList(final List<LocationLPN> locationLPNList) {
+        this.locationLPNList = locationLPNList;
+    }
+
     static List<LocationLPN> getEfficiencyLocationLPNList(final LocationLPNList locationLPNList, final Long itemId) {
         final List<LocationLPN> locationLPNS = listFrom(locationLPNList, itemId);
         return EfficiencyLocationLPNSorter.sort(
@@ -16,4 +23,28 @@ public record LocationLPNList(List<LocationLPN> locationLPNList) {
                 .filter(locationLPN -> locationLPN.getItemId().equals(itemId))
                 .toList();
     }
+
+    public List<LocationLPN> locationLPNList() {
+        return locationLPNList;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == this) return true;
+        if (null == obj || obj.getClass() != getClass()) return false;
+        final var that = (LocationLPNList) obj;
+        return Objects.equals(locationLPNList, that.locationLPNList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(locationLPNList);
+    }
+
+    @Override
+    public String toString() {
+        return "LocationLPNList[" +
+                "locationLPNList=" + locationLPNList + ']';
+    }
+
 }
